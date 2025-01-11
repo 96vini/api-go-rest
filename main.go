de578429a3b59791ec86e5e8a052bd611f227c1d
Vinícius Rosa <viniciusrosa@Viniciuss-MacBook-Air.local>
2025-01-11 20:15:01 -0300

    feat: add middlewares

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/96vini/api-go-rest/models"
+	"github.com/96vini/api-go-rest/database"
 	"github.com/96vini/api-go-rest/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "História 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "História 2"},
-	}
+	database.ConectaComBancoDeDados()
+	// models.Personalidades = []models.Personalidade{
+	// 	{Id: 1, Nome: "Nome 1", Historia: "História 1"},
+	// 	{Id: 2, Nome: "Nome 2", Historia: "História 2"},
+	// }
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 
 	"github.com/96vini/api-go-rest/controllers"
+	"github.com/96vini/api-go-rest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddlware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaUmaPersonalidade).Methods("Put")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
